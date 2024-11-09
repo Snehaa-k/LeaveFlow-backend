820e73cb5b4762b32a1f5336680c460299294fe9
sneha <snehaksnehak470@gmail.com>
2024-11-09 17:13:42 +0530

    first commit

---
+++ leaveapp/employe/urls.py
@@ -0,0 +1,24 @@
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+
+urlpatterns = [
+    path('register/', views.RegisterationApi.as_view(),name="register"),
+    path('verification/', views.VerifyOtp.as_view(),name="verification"),
+    path('login/',views.CustomTokenObtainPairView.as_view(),name='token_obtain_pair'),
+    path('leaveapply/',views.LeaveApply.as_view(),name='leave'),
+    path('leavelists/',views.ListLeave.as_view(),name='lists-leave'),
+    path('editleave/<int:id>/',views.EditLeave.as_view(),name='edit-leave'),
+    path('deleteleave/<int:id>/',views.leavedelete.as_view(),name='edit-leave'),
+    path('allists/',views.LeaveApplicationList.as_view(),name='list-leave'),
+    path('accept/<int:id>/',views.AcceptLeaveRequest.as_view(),name='leve-accept'),
+    path('reject/<int:id>/',views.RejectLeaveRequest.as_view(),name='leave-reject'),
+    path('calenderview/',views.DasghbordOverview.as_view(),name='leave-dashbord'),
+    path('profileupload/',views.ProfileImageUpload.as_view(),name='upload-image'),
+    path('profileview/',views.ProfileImageGet.as_view(),name='profile-view'),
+
+
+
+    
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
